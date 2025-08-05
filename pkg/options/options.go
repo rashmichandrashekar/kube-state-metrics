e6f412107b7fc01179180c28243a11219268010b
rashmichandrashekar <rashmy@microsoft.com>
2025-08-04 17:08:25 -0700

    Update pkg/options/options.go
    Co-authored-by: Pranshu Srivastava <prasriva@redhat.com>

--- pkg/options/options.go
+++ pkg/options/options.go
@@ -51,7 +51,7 @@ type Options struct {
 	Apiserver                               string   `yaml:"apiserver"`
 	CustomResourceConfig                    string   `yaml:"custom_resource_config"`
 	CustomResourceConfigFile                string   `yaml:"custom_resource_state_config_file"`
-	ContinueWithoutCustomResourceConfigFile bool     `yaml:"continue_without_custom_resource_config_file"`
+	ContinueWithoutCustomResourceConfigFile bool     `yaml:"continue_without_custom_resource_state_config_file"`
 	Host                                    string   `yaml:"host"`
 	Kubeconfig                              string   `yaml:"kubeconfig"`
 	Namespace                               string   `yaml:"namespace"`
